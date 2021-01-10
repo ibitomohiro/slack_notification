58b3743664f7469cc7a301e3291cb3aad44ef8a5
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-01-10 18:09:26 +0900

    実装完了

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
+gem 'clockwork'
 gem 'dotenv'
 gem 'slack-notify'
--- notification.rb
+++ notification.rb
@@ -1,3 +1,26 @@
+require 'clockwork'
 require 'dotenv/load'
 require 'slack-notify'
-client = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK_URL'])
+include Clockwork
+
+client = SlackNotify::Client.new(
+  webhook_url: ENV['WEBHOOK_URL'],
+  channel: "	#dic-kadai",
+  username: "mybot",
+  icon_url: "http://mydomain.com/myimage.png",
+  icon_emoji: ":shipit:",
+  link_names: 1
+)
+
+# client.notify("Hello World!")
+# puts "slack message is sent"
+every(3.second, 'slack') do
+  client.notify('こんにちは')
+end
+
+
+# handler do 
+#   3.times { client.notify("Hello World!") }
+# end
+
+# every(3.seconds, 'handler.job')
